b9a9535c30dd14a791d46575957b4e54e9e2b787
Sergey <sweid531@gmail.com>
2021-05-12 02:02:16 +0300

    Init

---
+++ app/src/main/java/com/sergeybogdanec/jetpackcomposeinvestigation/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.sergeybogdanec.jetpackcomposeinvestigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.sergeybogdanec.jetpackcomposeinvestigation.ui.theme.JetpackComposeInvestigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeInvestigationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeInvestigationTheme {
+        Greeting("Android")
+    }
+}
